3ab8044eb815244a3dc2aed5e5923b205371847a
Gabriel Torres Andreassa <70169977+TorresUTF@users.noreply.github.com>
2022-03-04 16:54:32 -0300

    ok

---
+++ pages/index.jsx
@@ -0,0 +1,27 @@
+
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <nav className='navbar'>
+        <ul className='navbar-bar'>
+          <li className='link-item'>
+            <a className='link-text' href="#">oi</a>
+          </li>
+          <li className='link-item'>
+            <a className='link-text' href="#">oi</a>
+          </li>
+          <li className='link-item'>
+            <a className='link-text' href="#">oi</a>
+          </li>
+          <li className='link-item'>
+            <a className='link-text' href="#">last</a>
+          </li>
+        </ul>
+      </nav>
+
+      <main></main>
+    </div>
+  )
+}
