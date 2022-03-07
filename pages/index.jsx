083f8aef398b09c722c7e471c1040d3941a5e6b5
Gabriel Torres Andreassa <70169977+TorresUTF@users.noreply.github.com>
2022-03-07 17:25:18 -0300

    Nicole

--- pages/index.jsx
+++ pages/index.jsx
@@ -7,21 +7,46 @@ export default function Home() {
       <nav className='navbar'>
         <ul className='navbar-bar'>
           <li className='nav-item'>
-            <a className='nav-link' href="#">👉</a>
+            <a className='nav-link' href="#">
+              <h1>👉</h1>
+              <span className='link-text'>Pro lado</span>
+            </a>
           </li>
           <li className='nav-item'>
-            <a className='nav-link' href="#">✌️</a>
+            <a className='nav-link' href="#">
+              <h1>✌️</h1>
+              <span className='link-text'>Paz e amor</span>
+            </a>
+            
           </li>
           <li className='nav-item'>
-            <a className='nav-link' href="#">💪</a>
+            <a className='nav-link' href="#">
+              <h1>💪</h1>              
+              <span className='link-text'>Birlllll</span>
+            </a>
           </li>
           <li className='nav-item'>
-            <a className='nav-link' href="#">🤟</a>
+            <a className='nav-link' href="#">
+              <h1>❤️</h1>
+              <span className='link-text'>Nicole</span>
+            </a>
+          </li>
+          <li className='nav-item'>
+            <a className='nav-link' href="#">
+              <h1>🤟</h1>
+              <span className='link-text'>Rock n Roll</span>
+            </a>          
           </li>
         </ul>
       </nav>
 
-      <main></main>
+      <main>
+        <div className='lado'></div>
+        <div className='paz'></div>
+        <div className='birl'></div>
+        <div className='heart'></div>
+        <div className='swag'></div>
+      </main>
     </div>
   )
 }
